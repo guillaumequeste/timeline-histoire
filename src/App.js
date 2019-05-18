86810058c7d87c22a7922cbdf27c157c2a860e6d
Guillaume Queste <guillaume.queste@laposte.net>
2019-05-18 10:34:43 +0200

    ajout

--- src/App.js
+++ src/App.js
@@ -143,7 +143,7 @@ const App = () => (
 
       <VerticalTimelineElement
         className="vertical-timeline-element--work"
-        date="1958 - 1969"
+        date="1959 - 1969"
         iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
       >
         <div className="badge">
@@ -160,72 +160,360 @@ const App = () => (
         </div>
       </VerticalTimelineElement>
 
-      
-  <VerticalTimelineElement
-    className="vertical-timeline-element--work"
-    date="2008 - 2010"
-    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-   
-  >
-    <h3 className="vertical-timeline-element-title">Web Designer</h3>
-    <h4 className="vertical-timeline-element-subtitle">Los Angeles, CA</h4>
-    <p>
-      User Experience, Visual Design
-    </p>
-  </VerticalTimelineElement>
-  <VerticalTimelineElement
-    className="vertical-timeline-element--work"
-    date="2006 - 2008"
-    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-    
-  >
-    <h3 className="vertical-timeline-element-title">Web Designer</h3>
-    <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
-    <p>
-      User Experience, Visual Design
-    </p>
-  </VerticalTimelineElement>
-  <VerticalTimelineElement
-    className="vertical-timeline-element--education"
-    date="April 2013"
-    iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-   
-  >
-    <h3 className="vertical-timeline-element-title">Content Marketing for Web, Mobile and Social Media</h3>
-    <h4 className="vertical-timeline-element-subtitle">Online Course</h4>
-    <p>
-      Strategy, Social Media
-    </p>
-  </VerticalTimelineElement>
-  <VerticalTimelineElement
-    className="vertical-timeline-element--education"
-    date="November 2012"
-    iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-   
-  >
-    <h3 className="vertical-timeline-element-title">Agile Development Scrum Master</h3>
-    <h4 className="vertical-timeline-element-subtitle">Certification</h4>
-    <p>
-      Creative Direction, User Experience, Visual Design
-    </p>
-  </VerticalTimelineElement>
-  <VerticalTimelineElement
-    className="vertical-timeline-element--education"
-    date="2002 - 2006"
-    iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-   
-  >
-    <h3 className="vertical-timeline-element-title">Bachelor of Science in Interactive Digital Media Visual Imaging</h3>
-    <h4 className="vertical-timeline-element-subtitle">Bachelor Degree</h4>
-    <p>
-      Creative Direction, Visual Design
-    </p>
-  </VerticalTimelineElement>
-  <VerticalTimelineElement
-    iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
-   
-  />
-</VerticalTimeline>
+      <VerticalTimelineElement
+        className="vertical-timeline-element--work"
+        date="1954 - 1959"
+        iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+      >
+        <div className="badge">
+          <div className="titres">
+            <h3 className="vertical-timeline-element-title">René Coty</h3>
+            <h4 className="vertical-timeline-element-subtitle">Président de la République</h4>
+            <p>
+              2ème président de la IV ème République
+            </p>
+          </div>
+          <div className="img">
+            <img src={require("./img/rene_coty.jpg")} alt="rene_coty"></img>
+          </div>
+        </div>
+      </VerticalTimelineElement>
+
+      <VerticalTimelineElement
+        className="vertical-timeline-element--work"
+        date="1947 - 1954"
+        iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+      >
+        <div className="badge">
+          <div className="titres">
+            <h3 className="vertical-timeline-element-title">Vincent Auriol</h3>
+            <h4 className="vertical-timeline-element-subtitle">Président de la République</h4>
+            <p>
+              1er président de la IV ème République
+            </p>
+          </div>
+          <div className="img">
+            <img src={require("./img/vincent_auriol.png")} alt="vincent_auriol"></img>
+          </div>
+        </div>
+      </VerticalTimelineElement>
+
+      <VerticalTimelineElement
+        className="vertical-timeline-element--work"
+        date="septembre 1944 - janvier 1947"
+        iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+      >
+        <div className="badge">
+          <div className="titres">
+            <h3 className="vertical-timeline-element-title">Gouvernement provisoire de la République française</h3>
+            <h4 className="vertical-timeline-element-subtitle">(GPRF)</h4>
+            <p>
+              Régime de Vichy
+            </p>
+          </div>
+        </div>
+      </VerticalTimelineElement>
+
+      <VerticalTimelineElement
+        className="vertical-timeline-element--work"
+        date="juin 1943 - 9 septembre 1944"
+        iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+      >
+        <div className="badge">
+          <div className="titres">
+            <h3 className="vertical-timeline-element-title">Comité français de la Libération nationale</h3>
+            <p>
+              Régime de Vichy
+            </p>
+          </div>
+        </div>
+      </VerticalTimelineElement>
+
+      <VerticalTimelineElement
+        className="vertical-timeline-element--work"
+        date="octobre 1940 - juin 1943"
+        iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+      >
+        <div className="badge">
+          <div className="titres">
+            <h3 className="vertical-timeline-element-title">Conseil de défense de l'Empire et Comité national français</h3>
+          </div>
+        </div>
+      </VerticalTimelineElement>
+
+      <VerticalTimelineElement
+        className="vertical-timeline-element--work"
+        date="1932 - 1940"
+        iconStyle={{ background: 'rgb(189, 60, 50)', color: '#fff' }}
+      >
+        <div className="badge">
+          <div className="titres">
+            <h3 className="vertical-timeline-element-title">Albert Lebrun</h3>
+            <h4 className="vertical-timeline-element-subtitle">Président de la République</h4>
+            <p>
+              14ème président de la III ème République
+            </p>
+          </div>
+          <div className="img">
+            <img src={require("./img/albert_lebrun.jpg")} alt="albert_lebrun"></img>
+          </div>
+        </div>
+      </VerticalTimelineElement>
+
+      <VerticalTimelineElement
+        className="vertical-timeline-element--work"
+        date="1931 - 1932"
+        iconStyle={{ background: 'rgb(189, 60, 50)', color: '#fff' }}
+      >
+        <div className="badge">
+          <div className="titres">
+            <h3 className="vertical-timeline-element-title">Paul Doumer</h3>
+            <h4 className="vertical-timeline-element-subtitle">Président de la République</h4>
+            <p>
+              13ème président de la III ème République
+            </p>
+          </div>
+          <div className="img">
+            <img src={require("./img/paul_doumer.jpg")} alt="paul_doumer"></img>
+          </div>
+        </div>
+      </VerticalTimelineElement>
+
+      <VerticalTimelineElement
+        className="vertical-timeline-element--work"
+        date="1924 - 1931"
+        iconStyle={{ background: 'rgb(189, 60, 50)', color: '#fff' }}
+      >
+        <div className="badge">
+          <div className="titres">
+            <h3 className="vertical-timeline-element-title">Gaston Doumergue</h3>
+            <h4 className="vertical-timeline-element-subtitle">Président de la République</h4>
+            <p>
+              12ème président de la III ème République
+            </p>
+          </div>
+          <div className="img">
+            <img src={require("./img/gaston_doumergue.jpg")} alt="gaston_doumergue"></img>
+          </div>
+        </div>
+      </VerticalTimelineElement>
+
+      <VerticalTimelineElement
+        className="vertical-timeline-element--work"
+        date="1920 - 1924"
+        iconStyle={{ background: 'rgb(189, 60, 50)', color: '#fff' }}
+      >
+        <div className="badge">
+          <div className="titres">
+            <h3 className="vertical-timeline-element-title">Alexandre Millerand</h3>
+            <h4 className="vertical-timeline-element-subtitle">Président de la République</h4>
+            <p>
+              11ème président de la III ème République
+            </p>
+          </div>
+          <div className="img">
+            <img src={require("./img/alexandre_millerand.jpg")} alt="alexandre_millerand"></img>
+          </div>
+        </div>
+      </VerticalTimelineElement>
+
+      <VerticalTimelineElement
+        className="vertical-timeline-element--work"
+        date="1920"
+        iconStyle={{ background: 'rgb(189, 60, 50)', color: '#fff' }}
+      >
+        <div className="badge">
+          <div className="titres">
+            <h3 className="vertical-timeline-element-title">Paul Deschanel</h3>
+            <h4 className="vertical-timeline-element-subtitle">Président de la République</h4>
+            <p>
+              10ème président de la III ème République
+            </p>
+          </div>
+          <div className="img">
+            <img src={require("./img/paul_deschanel.jpg")} alt="paul_deschanel"></img>
+          </div>
+        </div>
+      </VerticalTimelineElement>
+
+      <VerticalTimelineElement
+        className="vertical-timeline-element--work"
+        date="1913 - 1920"
+        iconStyle={{ background: 'rgb(189, 60, 50)', color: '#fff' }}
+      >
+        <div className="badge">
+          <div className="titres">
+            <h3 className="vertical-timeline-element-title">Raymond Poincaré</h3>
+            <h4 className="vertical-timeline-element-subtitle">Président de la République</h4>
+            <p>
+              9ème président de la III ème République
+            </p>
+          </div>
+          <div className="img">
+            <img src={require("./img/raymond_poincare.jpg")} alt="raymond_poincare"></img>
+          </div>
+        </div>
+      </VerticalTimelineElement>
+
+      <VerticalTimelineElement
+        className="vertical-timeline-element--work"
+        date="1906 - 1913"
+        iconStyle={{ background: 'rgb(189, 60, 50)', color: '#fff' }}
+      >
+        <div className="badge">
+          <div className="titres">
+            <h3 className="vertical-timeline-element-title">Armand Fallières</h3>
+            <h4 className="vertical-timeline-element-subtitle">Président de la République</h4>
+            <p>
+              8ème président de la III ème République
+            </p>
+          </div>
+          <div className="img">
+            <img src={require("./img/armand_fallieres.jpg")} alt="armand_fallieres"></img>
+          </div>
+        </div>
+      </VerticalTimelineElement>
+
+      <VerticalTimelineElement
+        className="vertical-timeline-element--work"
+        date="1899 - 1906"
+        iconStyle={{ background: 'rgb(189, 60, 50)', color: '#fff' }}
+      >
+        <div className="badge">
+          <div className="titres">
+            <h3 className="vertical-timeline-element-title">Emile Loubet</h3>
+            <h4 className="vertical-timeline-element-subtitle">Président de la République</h4>
+            <p>
+              7ème président de la III ème République
+            </p>
+          </div>
+          <div className="img">
+            <img src={require("./img/emile_loubet.jpg")} alt="emile_loubet"></img>
+          </div>
+        </div>
+      </VerticalTimelineElement>
+
+      <VerticalTimelineElement
+        className="vertical-timeline-element--work"
+        date="1895 - 1899"
+        iconStyle={{ background: 'rgb(189, 60, 50)', color: '#fff' }}
+      >
+        <div className="badge">
+          <div className="titres">
+            <h3 className="vertical-timeline-element-title">Félix Faure</h3>
+            <h4 className="vertical-timeline-element-subtitle">Président de la République</h4>
+            <p>
+              6ème président de la III ème République
+            </p>
+          </div>
+          <div className="img">
+            <img src={require("./img/felix_faure.jpg")} alt="felix_faure"></img>
+          </div>
+        </div>
+      </VerticalTimelineElement>
+
+      <VerticalTimelineElement
+        className="vertical-timeline-element--work"
+        date="1894 - 1895"
+        iconStyle={{ background: 'rgb(189, 60, 50)', color: '#fff' }}
+      >
+        <div className="badge">
+          <div className="titres">
+            <h3 className="vertical-timeline-element-title">Jean Casimir-Perier</h3>
+            <h4 className="vertical-timeline-element-subtitle">Président de la République</h4>
+            <p>
+              5ème président de la III ème République
+            </p>
+          </div>
+          <div className="img">
+            <img src={require("./img/perier.jpg")} alt="perier"></img>
+          </div>
+        </div>
+      </VerticalTimelineElement>
+
+      <VerticalTimelineElement
+        className="vertical-timeline-element--work"
+        date="1887 - 1894"
+        iconStyle={{ background: 'rgb(189, 60, 50)', color: '#fff' }}
+      >
+        <div className="badge">
+          <div className="titres">
+            <h3 className="vertical-timeline-element-title">Sadi Carnot</h3>
+            <h4 className="vertical-timeline-element-subtitle">Président de la République</h4>
+            <p>
+              4ème président de la III ème République
+            </p>
+          </div>
+          <div className="img">
+            <img src={require("./img/sadi_carnot.jpg")} alt="sadi_carnot"></img>
+          </div>
+        </div>
+      </VerticalTimelineElement>
+
+      <VerticalTimelineElement
+        className="vertical-timeline-element--work"
+        date="1879 - 1887"
+        iconStyle={{ background: 'rgb(189, 60, 50)', color: '#fff' }}
+      >
+        <div className="badge">
+          <div className="titres">
+            <h3 className="vertical-timeline-element-title">Jules Grévy</h3>
+            <h4 className="vertical-timeline-element-subtitle">Président de la République</h4>
+            <p>
+              3ème président de la III ème République
+            </p>
+          </div>
+          <div className="img">
+            <img src={require("./img/jules_grevy.jpg")} alt="jules_grevy"></img>
+          </div>
+        </div>
+      </VerticalTimelineElement>
+
+      <VerticalTimelineElement
+        className="vertical-timeline-element--work"
+        date="1873 - 1879"
+        iconStyle={{ background: 'rgb(189, 60, 50)', color: '#fff' }}
+      >
+        <div className="badge">
+          <div className="titres">
+            <h3 className="vertical-timeline-element-title">Patrice de Mac Mahon</h3>
+            <h4 className="vertical-timeline-element-subtitle">Président de la République</h4>
+            <p>
+              2ème président de la III ème République
+            </p>
+          </div>
+          <div className="img">
+            <img src={require("./img/mac_mahon.jpg")} alt="mac_mahon"></img>
+          </div>
+        </div>
+      </VerticalTimelineElement>
+
+      <VerticalTimelineElement
+        className="vertical-timeline-element--work"
+        date="1871 - 1873"
+        iconStyle={{ background: 'rgb(189, 60, 50)', color: '#fff' }}
+      >
+        <div className="badge">
+          <div className="titres">
+            <h3 className="vertical-timeline-element-title">Adolphe Thiers</h3>
+            <h4 className="vertical-timeline-element-subtitle">Président de la République</h4>
+            <p>
+              Chef du pouvoir exécutif (1871) puis 1er président de la III ème République (1871 - 1873)
+            </p>
+          </div>
+          <div className="img">
+            <img src={require("./img/adolphe_thiers.jpg")} alt="adolphe_thiers"></img>
+          </div>
+        </div>
+      </VerticalTimelineElement>
+
+  
+
+      <VerticalTimelineElement
+        iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
+      />
+
+    </VerticalTimeline>
   </>
 )
 
